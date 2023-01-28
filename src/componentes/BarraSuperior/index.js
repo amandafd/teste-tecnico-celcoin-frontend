8c1c3537281ffabf833b97d5f187dbb11983bf37
amandafd <amanda_dionisio@hotmail.com>
2023-01-27 23:50:21 -0300

    feat: updating barra superior

--- src/componentes/BarraSuperior/index.js
+++ src/componentes/BarraSuperior/index.js
@@ -9,16 +9,16 @@ const BarraSuperior = () => {
         <div className={styles.barra}>
             <CampoBusca />
             <div className={styles.opcoes}>
-                <Icon icon="material-symbols:settings" className={styles.icone__settings} />
+                <Icon icon="material-symbols:settings" className={styles.icone__settings} width="1.25rem"/>
                 <div className={styles.icone__notification}>
-                    <Icon icon="zondicons:notification" className={styles.icone__notificationSino} />
-                    <Icon icon="material-symbols:circle" className={styles.icone__notificationCirculo}/>
+                    <Icon icon="zondicons:notification" className={styles.icone__notificationSino} width="1rem"/>
+                    <Icon icon="material-symbols:circle" className={styles.icone__notificationCirculo} width="0.38rem" />
                 </div>
                 <div className={styles.olaUsuario}>
-                    <p className={styles.olaUsuarioTexto}><span>Olá,</span> Felipe</p>
+                    <p className={styles.olaUsuarioTexto}>Olá, <span>Felipe</span></p>
                     <img src={fotoFelipe} alt="Foto do usuário" className={styles.olaUsuarioFoto}/>
                 </div>
-                <img src={squareIcon} alt="Icone quadrado" className={styles.squareIcon}/>
+                <img src={squareIcon} alt="Icone quadrado" className={styles.squareIcon} width="1.25rem" />
             </div>
             
         </div>
