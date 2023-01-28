d134489599ab42e6c1f93dfa18d89c5fcb45067c
amandafd <amanda_dionisio@hotmail.com>
2023-01-27 22:42:46 -0300

    feat: changing divLateral

--- src/componentes/Logo/index.js
+++ src/componentes/Logo/index.js
@@ -1,9 +1,10 @@
 import styles from'./Logo.module.css';
+import VetorLogo from "../../assets/imagens/vetor-ident-logo.png"
 
 const Logo = () => {
     return (
         <div className={styles.logo}>
-            <img src="/imagens/vetor-ident-logo.png" alt="Vetor do logo IDENTLOGO" className={styles.logo__vetor} />
+            <img src={VetorLogo} alt="Vetor do logo IDENTLOGO" className={styles.logo__vetor} />
             <p className={styles.logo__nome}><span >IDENT</span>LOGO</p>
         </div>
     ) 
